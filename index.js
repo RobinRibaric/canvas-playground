8a88bc9ea2d12ced5856e594563c155a375f64f4
RobinRibaric <zakariasribaric@hotmail.com>
2022-09-25 13:08:58 +0200

    colorize enemies and after image effect

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext("2d");
 
-const player = new Player(canvas.width / 2, canvas.height / 2, 30, "blue");
+const player = new Player(canvas.width / 2, canvas.height / 2, 30, "white");
 //const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', {x: 1, y: 1,});
 
 const projectiles = [];
@@ -18,13 +18,15 @@ spawnEnemies();
 function animate() {
    
     animationId = requestAnimationFrame(animate);
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = "rgba(0,0,0,0.1)";
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+    //ctx.clearRect(0,0,canvas.width,canvas.height);
     player.draw(ctx);
 
     projectiles.forEach((projectile, projectileIndex) => {
         projectile.update(ctx);
         //removing projectiles when outside the screen
-        if(projectile.x + projectile.radius < 0 || projectile.x - projectile.radius < canvas.width ||
+        if(projectile.x + projectile.radius < 0 || projectile.x - projectile.radius > canvas.width ||
             projectile.y + projectile.radius < 0 || projectile.y - projectile.radius > canvas.height) {
             setTimeout(() => {
                 projectiles.splice(projectileIndex, 1);
@@ -62,9 +64,10 @@ function animate() {
 }
 
 window.addEventListener("click", (event) => {
+    const speed = 5;
     const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
-    const velocity = {x: Math.cos(angle), y: Math.sin(angle)};
-    const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity);
+    const velocity = {x: Math.cos(angle) * speed, y: Math.sin(angle) * speed};
+    const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity);
     projectiles.push(projectile);
 });
 function spawnEnemies() {
@@ -82,7 +85,8 @@ function spawnEnemies() {
             y = Math.random() < 0.5 ? 0 - radius :  canvas.height + radius;
         }
         
-        const color = "yellow";
+        
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
         const angle = Math.atan2( canvas.height / 2 - y, canvas.width / 2 - x);
         const velocity = {x: Math.cos(angle), y: Math.sin(angle)};
         enemies.push(new Enemy(x, y, radius, color, velocity))
