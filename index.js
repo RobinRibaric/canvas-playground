00cfe87ef0a3c595cb8e94b686f08b9a25555c83
RobinRibaric <zakariasribaric@hotmail.com>
2022-09-25 12:55:16 +0200

    removing projectiles of screen

--- index.js
+++ index.js
@@ -10,29 +10,51 @@ const player = new Player(canvas.width / 2, canvas.height / 2, 30, "blue");
 
 const projectiles = [];
 const enemies = [];
+let animationId;
 
 animate()
 spawnEnemies();
+
 function animate() {
    
-    requestAnimationFrame(animate);
+    animationId = requestAnimationFrame(animate);
     ctx.clearRect(0,0,canvas.width,canvas.height);
     player.draw(ctx);
 
-    projectiles.forEach((projectile) => {
+    projectiles.forEach((projectile, projectileIndex) => {
         projectile.update(ctx);
+        //removing projectiles when outside the screen
+        if(projectile.x + projectile.radius < 0 || projectile.x - projectile.radius < canvas.width ||
+            projectile.y + projectile.radius < 0 || projectile.y - projectile.radius > canvas.height) {
+            setTimeout(() => {
+                projectiles.splice(projectileIndex, 1);
+            }, 0)
+        }
     });
     enemies.forEach((enemy, enemyIndex) => {
         enemy.update(ctx);
+        const distance = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+        if(distance - player.radius - enemy.radius < 1) {
+            cancelAnimationFrame(animationId);
+
+         /*    setTimeout(() => {
+                enemies.splice(enemyIndex, 1);
+                cancelAnimationFrame(animationId);
+            }, 0); */
+            console.log("collision detected");
+        }
+
 
         projectiles.forEach((projectile, projectileIndex) => {
             const distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
             console.log(distance);
 
             if(distance -enemy.radius - projectile.radius < 1) {
+                setTimeout(() => {
+                    enemies.splice(enemyIndex, 1);
+                    projectiles.splice(projectileIndex, 1);
+                }, 0);
                 console.log("collision detected");
-                enemies.splice(enemyIndex, 1);
-                projectiles.splice(projectileIndex, 1);
             }
         });
     });
@@ -45,9 +67,8 @@ window.addEventListener("click", (event) => {
     const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity);
     projectiles.push(projectile);
 });
-
 function spawnEnemies() {
-   // setInterval(() => {
+   setInterval(() => {
         const radius = Math.random() * (30 - 8) + 8;
         let x = 0;
         let y = 0;
@@ -65,5 +86,5 @@ function spawnEnemies() {
         const angle = Math.atan2( canvas.height / 2 - y, canvas.width / 2 - x);
         const velocity = {x: Math.cos(angle), y: Math.sin(angle)};
         enemies.push(new Enemy(x, y, radius, color, velocity))
-    //}, 1000);
+    }, 1000);
 }
