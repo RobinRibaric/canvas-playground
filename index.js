13b79244868e44b009a21f765ce352fb51f01720
RobinRibaric <zakariasribaric@hotmail.com>
2022-09-25 12:38:41 +0200

    collision-detection

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const player = new Player(canvas.width / 2, canvas.height / 2, 30, "blue");
 //const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', {x: 1, y: 1,});
 
 const projectiles = [];
+const enemies = [];
 
 animate()
+spawnEnemies();
 function animate() {
    
     requestAnimationFrame(animate);
@@ -19,6 +21,20 @@ function animate() {
 
     projectiles.forEach((projectile) => {
         projectile.update(ctx);
+    });
+    enemies.forEach((enemy, enemyIndex) => {
+        enemy.update(ctx);
+
+        projectiles.forEach((projectile, projectileIndex) => {
+            const distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+            console.log(distance);
+
+            if(distance -enemy.radius - projectile.radius < 1) {
+                console.log("collision detected");
+                enemies.splice(enemyIndex, 1);
+                projectiles.splice(projectileIndex, 1);
+            }
+        });
     });
 
 }
@@ -29,3 +45,25 @@ window.addEventListener("click", (event) => {
     const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity);
     projectiles.push(projectile);
 });
+
+function spawnEnemies() {
+   // setInterval(() => {
+        const radius = Math.random() * (30 - 8) + 8;
+        let x = 0;
+        let y = 0;
+        
+        console.log("go");
+        if(Math.random() < 0.5) {
+             x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;//Math.random() * canvas.width;
+             y = Math.random() * canvas.height//Math.random() < 0.5 ? 0 - radius :  canvas.height + radius;
+        } else {
+            x = Math.random() * canvas.width;//Math.random() * canvas.width;
+            y = Math.random() < 0.5 ? 0 - radius :  canvas.height + radius;
+        }
+        
+        const color = "yellow";
+        const angle = Math.atan2( canvas.height / 2 - y, canvas.width / 2 - x);
+        const velocity = {x: Math.cos(angle), y: Math.sin(angle)};
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+    //}, 1000);
+}
